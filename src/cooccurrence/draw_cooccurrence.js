2f52448a6c4ad323c1dfab3db4f563b01512f63e
annalavrenkoepfl <anna.lavrenko@epfl.ch>
2025-04-18 15:19:35 +0200

    get just script without changing author

--- src/cooccurrence/draw_cooccurrence.js
+++ src/cooccurrence/draw_cooccurrence.js
@@ -1,210 +1,109 @@
-<html class="ocks-org do-not-copy">
 
-<head>
-  <meta charset="utf-8">
-  <title>Les Misérables Co-occurrence</title>
-  <style>
-    @import url(../style.css?aea6f0a);
+var margin = { top: 80, right: 0, bottom: 10, left: 80 },
+  width = 720,
+  height = 720;
 
-    .background {
-      fill: #eee;
-    }
+var x = d3.scale.ordinal().rangeBands([0, width]),
+  z = d3.scale.linear().domain([0, 4]).clamp(true),
+  color = d3.scale.linear()
+    .domain([0, 7, 15]) // tweak to your data
+    .range(["green", "yellow", "red"]);
 
-    line {
-      stroke: #fff;
-    }
+var svg = d3.select("body").append("svg")
+  .attr("width", width + margin.left + margin.right)
+  .attr("height", height + margin.top + margin.bottom)
+  .style("margin-left", -margin.left + "px")
+  .append("g")
+  .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
-    text.active {
-      fill: red;
-    }
-  </style>
-  <script type="text/javascript" async=""
-    src="https://www.googletagmanager.com/gtag/js?id=G-826WZPS36G&amp;cx=c&amp;_slc=1"></script>
-  <script src="//d3js.org/d3.v2.min.js" charset="utf-8"></script>
+d3.json("naruto.json", function (miserables) {
+  var matrix = [],
+    nodes = miserables.nodes,
+    n = nodes.length;
 
-</head>
+  // Compute index per node.
+  nodes.forEach(function (node, i) {
+    node.index = i;
+    node.count = 0;
+    matrix[i] = d3.range(n).map(function (j) { return { x: j, y: i, z: 0 }; });
+  });
 
-<body>
-  <header>
-    <aside>April 10, 2012</aside>
-    <a href="../" rel="author">Mike Bostock</a>
-  </header>
+  // Convert links to matrix; count character occurrences.
+  miserables.links.forEach(function (link) {
+    matrix[link.source][link.target].z += link.value;
+    matrix[link.target][link.source].z += link.value;
+    matrix[link.source][link.source].z += link.value;
+    matrix[link.target][link.target].z += link.value;
+    nodes[link.source].count += link.value;
+    nodes[link.target].count += link.value;
+  });
 
-  <h1><i>Les Misérables</i> Co-occurrence</h1>
+  // Precompute the orders.
+  var orders = {
+    count: d3.range(n).sort(function (a, b) { return nodes[b].count - nodes[a].count; }),
+  };
 
-  <aside style="margin-top:80px;">
-    </p>
-    <p>This matrix diagram visualizes character co-occurrences in Victor Hugo’s <i><a
-          href="https://en.wikipedia.org/wiki/Les_Misérables">Les Misérables</a></i>.
+  // The default sort order.
+  x.domain(orders.count);
 
-    </p>
-    <p>Each colored cell represents two characters that appeared in the same chapter; darker cells indicate characters
-      that co-occurred more frequently.
+  svg.append("rect")
+    .attr("class", "background")
+    .attr("width", width)
+    .attr("height", height);
 
-    </p>
-    <p>Use the drop-down menu to reorder the matrix and explore the data.
+  var row = svg.selectAll(".row")
+    .data(matrix)
+    .enter().append("g")
+    .attr("class", "row")
+    .attr("transform", function (d, i) { return "translate(0," + x(i) + ")"; })
+    .each(row);
 
-    </p>
-    <p>Built with <a href="http://d3js.org/">d3.js</a>.
-    </p>
-  </aside>
+  row.append("line")
+    .attr("x2", width);
 
-  <script>
+  row.append("text")
+    .attr("x", -6)
+    .attr("y", x.rangeBand() / 2)
+    .attr("dy", ".32em")
+    .attr("text-anchor", "end")
+    .text(function (d, i) { return nodes[i].name; });
 
-    var margin = { top: 80, right: 0, bottom: 10, left: 80 },
-      width = 720,
-      height = 720;
+  var column = svg.selectAll(".column")
+    .data(matrix)
+    .enter().append("g")
+    .attr("class", "column")
+    .attr("transform", function (d, i) { return "translate(" + x(i) + ")rotate(-90)"; });
 
-    var x = d3.scale.ordinal().rangeBands([0, width]),
-      z = d3.scale.linear().domain([0, 4]).clamp(true),
-      color = d3.scale.linear()
-              .domain([0, 7, 15]) // tweak to your data
-              .range(["green", "yellow", "red"]);
+  column.append("line")
+    .attr("x1", -width);
 
-    var svg = d3.select("body").append("svg")
-      .attr("width", width + margin.left + margin.right)
-      .attr("height", height + margin.top + margin.bottom)
-      .style("margin-left", -margin.left + "px")
-      .append("g")
-      .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+  column.append("text")
+    .attr("x", 6)
+    .attr("y", x.rangeBand() / 2)
+    .attr("dy", ".32em")
+    .attr("text-anchor", "start")
+    .text(function (d, i) { return nodes[i].name; });
 
-    d3.json("naruto.json", function (miserables) {
-      var matrix = [],
-        nodes = miserables.nodes,
-        n = nodes.length;
+  function row(row) {
+    var cell = d3.select(this).selectAll(".cell")
+      .data(row.filter(function (d) { return d.z; }))
+      .enter().append("rect")
+      .attr("class", "cell")
+      .attr("x", function (d) { return x(d.x); })
+      .attr("width", x.rangeBand())
+      .attr("height", x.rangeBand())
+      .style("fill-opacity", function (d) { return z(d.z); })
+      .style("fill", function (d) { return color(d.z); })
+      .on("mouseover", mouseover)
+      .on("mouseout", mouseout);
+  }
 
-      // Compute index per node.
-      nodes.forEach(function (node, i) {
-        node.index = i;
-        node.count = 0;
-        matrix[i] = d3.range(n).map(function (j) { return { x: j, y: i, z: 0 }; });
-      });
+  function mouseover(p) {
+    d3.selectAll(".row text").classed("active", function (d, i) { return i == p.y; });
+    d3.selectAll(".column text").classed("active", function (d, i) { return i == p.x; });
+  }
 
-      // Convert links to matrix; count character occurrences.
-      miserables.links.forEach(function (link) {
-        matrix[link.source][link.target].z += link.value;
-        matrix[link.target][link.source].z += link.value;
-        matrix[link.source][link.source].z += link.value;
-        matrix[link.target][link.target].z += link.value;
-        nodes[link.source].count += link.value;
-        nodes[link.target].count += link.value;
-      });
-
-      // Precompute the orders.
-      var orders = {
-        count: d3.range(n).sort(function (a, b) { return nodes[b].count - nodes[a].count; }),
-      };
-
-      // The default sort order.
-      x.domain(orders.count);
-
-      svg.append("rect")
-        .attr("class", "background")
-        .attr("width", width)
-        .attr("height", height);
-
-      var row = svg.selectAll(".row")
-        .data(matrix)
-        .enter().append("g")
-        .attr("class", "row")
-        .attr("transform", function (d, i) { return "translate(0," + x(i) + ")"; })
-        .each(row);
-
-      row.append("line")
-        .attr("x2", width);
-
-      row.append("text")
-        .attr("x", -6)
-        .attr("y", x.rangeBand() / 2)
-        .attr("dy", ".32em")
-        .attr("text-anchor", "end")
-        .text(function (d, i) { return nodes[i].name; });
-
-      var column = svg.selectAll(".column")
-        .data(matrix)
-        .enter().append("g")
-        .attr("class", "column")
-        .attr("transform", function (d, i) { return "translate(" + x(i) + ")rotate(-90)"; });
-
-      column.append("line")
-        .attr("x1", -width);
-
-      column.append("text")
-        .attr("x", 6)
-        .attr("y", x.rangeBand() / 2)
-        .attr("dy", ".32em")
-        .attr("text-anchor", "start")
-        .text(function (d, i) { return nodes[i].name; });
-
-      function row(row) {
-        var cell = d3.select(this).selectAll(".cell")
-          .data(row.filter(function (d) { return d.z; }))
-          .enter().append("rect")
-          .attr("class", "cell")
-          .attr("x", function (d) { return x(d.x); })
-          .attr("width", x.rangeBand())
-          .attr("height", x.rangeBand())
-          .style("fill-opacity", function (d) { return z(d.z); })
-          .style("fill", function (d)  { return color(d.z); })
-          .on("mouseover", mouseover)
-          .on("mouseout", mouseout);
-      }
-
-      function mouseover(p) {
-        d3.selectAll(".row text").classed("active", function (d, i) { return i == p.y; });
-        d3.selectAll(".column text").classed("active", function (d, i) { return i == p.x; });
-      }
-
-      function mouseout() {
-        d3.selectAll("text").classed("active", false);
-      }
-    });
-
-  </script>
- 
-
-  <p class="attribution">Source: <a href="http://www-cs-staff.stanford.edu/~uno/sgb.html">The Stanford GraphBase</a>.
-
-  </p>
-  <p>A network can be represented by an <i><a href="https://en.wikipedia.org/wiki/Adjacency_matrix">adjacency
-        matrix</a></i>, where each cell <i>ij</i> represents an edge from vertex <i>i</i> to vertex <i>j</i>. Here,
-    vertices represent characters in a book, while edges represent co-occurrence in a chapter.
-
-  </p>
-  <p>Given this two-dimensional representation of a graph, a natural visualization is to show the matrix! However, the
-    effectiveness of a matrix diagram is heavily dependent on the order of rows and columns: if related nodes are placed
-    closed to each other, it is easier to identify clusters and bridges.
-
-  </p>
-  <p>This example lets you try different orderings via the drop-down menu. This type of diagram can be extended with
-    manual reordering of rows and columns, and expanding or collapsing of clusters, to allow deeper exploration. <a
-      href="https://en.wikipedia.org/wiki/Jacques_Bertin">Jacques Bertin</a> (or more specifically, his fleet of
-    assistants) did this by hand with paper strips.
-
-  </p>
-  <p>While path-following is harder in a matrix view than in a <a
-      href="https://mbostock.github.com/d3/ex/force.html">node-link diagram</a>, matrices have other advantages. As
-    networks get large and highly connected, node-link diagrams often devolve into giant hairballs of line crossings.
-    Line crossings are impossible with matrix views. Matrix cells can also be encoded to show additional data; here
-    color depicts clusters computed by a community-detection algorithm.
-
-  </p>
-  <p>Want more? See this analysis of <a href="../shuffle/compare.html">shuffling algorithms</a> using matrix diagrams.
-
-  </p>
-  <footer>
-    <aside>January 12, 2012</aside>
-    <a href="../" rel="author">Mike Bostock</a>
-  </footer>
-
-  <script>
-
-    GoogleAnalyticsObject = "ga", ga = function () { ga.q.push(arguments); }, ga.q = [], ga.l = +new Date;
-    ga("create", "UA-48272912-3", "ocks.org");
-    ga("send", "pageview");
-
-  </script>
-  <script async="" src="//www.google-analytics.com/analytics.js"></script>
-</body>
-
-</html>
+  function mouseout() {
+    d3.selectAll("text").classed("active", false);
+  }
+});
