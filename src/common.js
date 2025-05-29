7cd1a3e3e16e7d2951b6c80e810348953a29572d
Vladimir Melnikov <vladimir.melnikov@epfl.ch>
2025-05-29 17:25:32 +0200

    fix

--- src/common.js
+++ src/common.js
@@ -2,38 +2,38 @@
 //     fetch('./characters.json').then(res => res.json()),
 //     fetch('./locations.json').then(res => res.json())
 // ]);
-const characters = fetch('./characters.json').then(res => res.json()).catch(error => {
+const charactersP = fetch('./characters.json').then(res => res.json()).catch(error => {
     console.error("Error loading characters.json:", error);
 });
-const locations = fetch('./locations.json').then(res => res.json()).catch(error => {
+const locationsP = fetch('./locations.json').then(res => res.json()).catch(error => {
     console.error("Error loading locations.json:", error);
 });
 
-const nameToBirthplace = characters.then(charList => 
+const nameToBirthplaceP = charactersP.then(charList => 
   new Map(charList.map(c => [c.name, c.place_of_birth]))
 );
 
-const nameToAvatar = characters.then(charList => 
+const nameToAvatarP = charactersP.then(charList => 
    new Map(charList.map(c => [c.name, c.avatar]))
 )
-const locationMap = locations.then(locList => 
+const locationMapP = locationsP.then(locList => 
   new Map(locList.map(l => [l.location, { x: l.x, y: l.y }]))
 );
 
 
 async function buildCoordMap() {
-  const [nameToBirthplaceV, locationMapV] = await Promise.all([
-    nameToBirthplace,
-    locationMap
+  const [nameToBirthplace, locationMap] = await Promise.all([
+    nameToBirthplaceP,
+    locationMapP
   ]);
 
   const coordMap = new Map();
 
   nameSet.forEach(name => {
-    const birthplace = nameToBirthplaceV.get(name);
+    const birthplace = nameToBirthplace.get(name);
     if (!birthplace) return;
 
-    const coords = locationMapV.get(birthplace);
+    const coords = locationMap.get(birthplace);
     if (!coords) return;
 
     const key = `${coords.x},${coords.y}`;
@@ -46,4 +46,4 @@ async function buildCoordMap() {
   return coordMap;
 }
 
-const coordMap = buildCoordMap
+const coordMapP = buildCoordMap
