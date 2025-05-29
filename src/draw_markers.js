c03f4b387f415e3ced99c70e2d8e70ff992b55ef
Vladimir Melnikov <vladimir.melnikov@epfl.ch>
2025-05-29 14:42:32 +0200

    expanding avatar list on map

--- src/draw_markers.js
+++ src/draw_markers.js
@@ -1,3 +1,111 @@
+async function drawSmall(names, wrapper, nameToAvatar) {
+  const bodyHeight = 42 * names.length;
+  const body = document.createElement('div');
+  body.className = 'pin-body';
+  body.style.height = bodyHeight + 'px';
+  body.style.top = "0px";
+
+
+  for (let i = 0; i < names.length; i++) {
+    const avatar = document.createElement('div');
+    avatar.className = 'pin-avatar';
+    avatar.style.top = `${42 * (i)}px`;
+
+    const img = document.createElement('img');
+    img.src = nameToAvatar.get(names[i]);
+    img.alt = names[i];
+    img.title = names[i];
+
+    avatar.appendChild(img);
+    body.appendChild(avatar);
+  }
+
+  wrapper.appendChild(body);
+}
+
+async function drawBig(names, wrapper, nameToAvatar) {
+  const rows = Math.min(names.length, 5)
+  const cols = ((names.length + 4) / 5) | 0;
+  console.log('cols:', cols);
+
+  const bodyHeight = 42 * rows;
+  const bodyWidth = 42 * cols;
+
+  const body = document.createElement('div');
+  body.classList.add('pin-body', 'big-pin');
+  body.style.height = bodyHeight + 'px';
+  body.style.width = bodyWidth + 'px';
+  body.style.top = "0px";
+  body.style.zIndex = "11";
+
+  for (let i = 0; i < names.length; i++) {
+    const row = i % 5;
+    const col = (i / 5) | 0;
+
+    const avatar = document.createElement('div');
+    avatar.classList.add('pin-avatar', 'big-pin');
+    avatar.style.top = `${42 * row}px`;
+    avatar.style.left = `${42 * col}px`;
+
+    const img = document.createElement('img');
+    img.src = nameToAvatar.get(names[i]);
+    img.alt = names[i];
+    img.title = names[i];
+    img.zIndex = "12";
+
+    avatar.appendChild(img);
+    body.appendChild(avatar);
+  }
+  body.addEventListener('mouseleave', () => {
+    wrapper.querySelectorAll('.big-pin').forEach(el => el.remove());
+  }
+  )
+
+  wrapper.appendChild(body);
+}
+
+async function drawFolded(names, wrapper, nameToAvatar) {
+  const bodyHeight = 42 * 3;
+  const body = document.createElement('div');
+  body.className = 'pin-body';
+  body.style.height = bodyHeight + 'px';
+  body.style.top = "0px";
+
+  {
+    const avatar = document.createElement('div');
+    avatar.className = 'pin-avatar';
+    avatar.style.top = `0px`;
+
+    const img = document.createElement('img');
+    img.src = "./profile_pictures/manymore.png"
+    img.alt = "other";
+    img.title = "other";
+    img.addEventListener('mouseenter', () => {
+      drawBig(names, wrapper, nameToAvatar);
+    });
+
+
+    avatar.appendChild(img);
+    body.appendChild(avatar);
+  }
+
+  for (let i = 0; i < 2; i++) {
+    const avatar = document.createElement('div');
+    avatar.className = 'pin-avatar';
+    avatar.style.top = `${42 * (i + 1)}px`;
+
+    const img = document.createElement('img');
+    img.src = nameToAvatar.get(names[i]);
+    img.alt = names[i];
+    img.title = names[i];
+
+    avatar.appendChild(img);
+    body.appendChild(avatar);
+  }
+
+  wrapper.appendChild(body);
+}
+
 async function updateMap(nameSet) {
   const container = document.getElementById('image-container');
   container.querySelectorAll('.pin-wrapper').forEach(el => el.remove());
@@ -34,59 +142,26 @@ async function updateMap(nameSet) {
     const wrapper = document.createElement('div');
     wrapper.className = 'pin-wrapper';
 
-    const height = 42 * (names.length - 1);
-    wrapper.style.left = coords.x + 'px';
-    wrapper.style.top = coords.y - height + 'px';
-
-
-    // === CAP ===
-    // const cap = document.createElement('div');
-    // cap.className = 'pin-cap';
-
-    const circle = document.createElement('div');
-    circle.className = 'pin-cap-circle';
-
-    const avatar = document.createElement('div');
-    avatar.className = 'pin-avatar';
-
-    const topImg = document.createElement('img');
-    topImg.src = nameToAvatar.get(names[0]);
-    topImg.alt = names[0];
-    topImg.title = names[0];
-
-    avatar.appendChild(topImg);
-    circle.appendChild(avatar);
-    wrapper.appendChild(circle);
-
-    // === ORANGE BODY ===
-    if (names.length > 1) {
-      const bodyHeight = 42 * (names.length - 1);
-      const body = document.createElement('div');
-      body.className = 'pin-body';
-      body.style.height = bodyHeight + 'px';
-      body.style.top = "21px";
-
-      for (let i = 1; i < names.length; i++) {
-        const avatar = document.createElement('div');
-        avatar.className = 'pin-avatar';
-        avatar.style.top = `${42 * (i-1) + 21}px`;
-
-        const img = document.createElement('img');
-        img.src = nameToAvatar.get(names[i]);
-        img.alt = names[i];
-        img.title = names[i];
-
-        avatar.appendChild(img);
-        body.appendChild(avatar);
-      }
-
-      wrapper.appendChild(body);
-    }
-
-    // === TAIL ===
     const tail = document.createElement('div');
     tail.className = 'pin-tail';
-    tail.style.top = `${42 * Math.max(0, names.length - 1)}px`;
+
+    var height;
+    // === ORANGE BODY ===
+    if (names.length <= 3) {
+      await drawSmall(names, wrapper, nameToAvatar);
+      tail.style.top = `${42 * Math.max(0, names.length - 1)}px`;
+      height = 42 * (names.length - 1);
+    } else {
+      await drawFolded(names, wrapper, nameToAvatar);
+      // await drawBig(names, wrapper, nameToAvatar);
+      tail.style.top = `${42 * 2}px`;
+      height = 42 * 2;
+    }
+
+    wrapper.style.top = coords.y - height + 'px';
+    wrapper.style.left = coords.x + 'px';
+
+    // === TAIL ===
 
     wrapper.appendChild(tail);
     container.appendChild(wrapper);
